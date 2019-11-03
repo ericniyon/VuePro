3f85f00aa13359a719e36e220819617ed3d94247
madiba <niyoeri6@gmail.com>
2019-11-03 04:29:54 +0200

    my first vue application

---
+++ vueblog1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div >
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import AddBlog from './components/AddBlog.vue'
+import ListBlogs from './components/ListBlogs.vue'
+import Header from './components/Header.vue'
+
+
+export default {
+  components:{
+    'list-blog':ListBlogs,
+    'app-header':Header
+  },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style >
+
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold'
+}
+</style>
